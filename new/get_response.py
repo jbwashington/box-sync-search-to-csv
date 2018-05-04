7e81e189c06137c5d0f18617facdcd83666c0f3c
James Washington <jbwashington@gmail.com>
2018-05-03 21:48:28 -0400

    added html file for testing purposes

--- new/get_response.py
+++ new/get_response.py
@@ -1,16 +1,20 @@
 # get_response.py
-
+# ===============
 import requests, browsercookie
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# global variables
+# ================
 BASE_URL = 'https://cbs.app.box.com'
 URL =  'https://cbs.app.box.com/folder/0/search'
 QUERY = "*.pst"
 PAGE_SIZE = '1000'
-LINK_CLASS = 'div.item-details'
+PARENT_CSS = ['ol', 'file-list-body list-view']
 PAGE_NUMBER = '0'
 
+# functions
+# =========
 def get_response(url, params):
     """attach cookies to request and return body of GET request"""
     # dump session cookies from browser 
@@ -21,9 +25,10 @@ def get_response(url, params):
     response_body = response.content
     return response_body
 
-def extract_data(soup, selector):
+def extract_data(soup, div, css):
     """takes soup object and class of data to be extracted"""
-    result_list = soup.select(selector)
+    # result_list = soup.select(selector)
+    result_list = soup.find(div, attrs={ 'class' : css })
     return result_list
 
 def make_soup(response_body):
@@ -33,7 +38,6 @@ def make_soup(response_body):
 
 def send_payload(query, page_size='1000', page_number='0'):
     """takes a search parameter string and page number, returns dictionary with payload """
-
     payload = {
             'query': query, 
             'itemSize' : '', 
@@ -45,10 +49,7 @@ def send_payload(query, page_size='1000', page_number='0'):
             'folderID' : '0',
             'pageNumber' : page_number
             }
-
     return payload
-
-
 
 if __name__ == '__main__':
 
@@ -56,13 +57,9 @@ if __name__ == '__main__':
     payload = send_payload(QUERY, PAGE_SIZE, PAGE_NUMBER)
     response = get_response(URL, payload)
     soup_object = make_soup(response)
-    result_list = extract_data(soup_object, LINK_CLASS)
+    result_list = extract_data(soup_object, PARENT_CSS[0], PARENT_CSS[1])
 
-    print(result_list[0]) # !TODO: extract relevant info from this list 
+    print(result_list) # !TODO: extract relevant info from this list 
 
     print('=========================================')
-
-
-
-
 
