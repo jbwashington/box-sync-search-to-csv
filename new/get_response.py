cbd2119a0fa5accc9123e3f90a5b17db62507d3f
James Washington <jbwashington@gmail.com>
2018-05-15 21:02:47 -0400

    did some stuff idk

--- new/get_response.py
+++ new/get_response.py
@@ -1,20 +1,58 @@
 # get_response.py
-# ===============
+
 import requests, browsercookie
 from bs4 import BeautifulSoup
 import pandas as pd
 
 # global variables
 # ================
+
 BASE_URL = 'https://cbs.app.box.com'
-URL =  'https://cbs.app.box.com/folder/0/search'
+SUFFIX = '/folder/0/search'
+URL = BASE_URL + SUFFIX
 QUERY = "*.pst"
 PAGE_SIZE = '1000'
-PARENT_CSS = ['ol', 'file-list-body list-view']
 PAGE_NUMBER = '0'
+
+# CSS selectors
+# =============
+
+CONTENT_CSS = ['ol', 'file-list-body list-view']
+TITLE_CSS = ['div', 'item-name file-list-name']
+MODIFIED_CSS = ['div', 'item-modified-and-size']
+BREADCRUMBS_CSS = ['div', 'item-breadcrumbs']
 
 # functions
 # =========
+
+def extract_col(row):
+    """iterates over a beautifulsoup list of strings and returns all the rows"""
+    col = []
+    for x in row:
+        col[x] = extract_row(x)
+    return col
+    
+
+def extract_row(result_list):
+    """takes a list element in a soup element object and returns parsed data """
+    title = result_list.find('div', attrs = { 'class' : 'item-name file-list-name' }).text
+    modified = result_list.find('div', attrs = { 'class' : 'item-modified-and-size' }).text
+    breadcrumbs = result_list.find('div', attrs = { 'class' : 'item-breadcrumbs' }).text
+    href = BASE_URL + result_list.find('a')['href']
+    row = [title, modified, breadcrumbs, href]
+    return row
+    
+
+def extract_table(soup):
+    """takes soup object and class of data to be extracted"""
+    result_list = soup.find('ol', attrs = { 'class' : 'file-list-body list-view' })
+    return result_list
+
+def make_soup(response_body):
+    """takes response content and returns BeautifulSoup object"""
+    soup = BeautifulSoup(response_body, 'lxml')
+    return soup
+
 def get_response(url, params):
     """attach cookies to request and return body of GET request"""
     # dump session cookies from browser 
@@ -24,17 +62,6 @@ def get_response(url, params):
     # make soup out of response body
     response_body = response.content
     return response_body
-
-def extract_data(soup, div, css):
-    """takes soup object and class of data to be extracted"""
-    # result_list = soup.select(selector)
-    result_list = soup.find(div, attrs={ 'class' : css })
-    return result_list
-
-def make_soup(response_body):
-    """takes response content and returns BeautifulSoup object"""
-    soup = BeautifulSoup(response_body, 'lxml')
-    return soup
 
 def send_payload(query, page_size='1000', page_number='0'):
     """takes a search parameter string and page number, returns dictionary with payload """
@@ -57,9 +84,11 @@ if __name__ == '__main__':
     payload = send_payload(QUERY, PAGE_SIZE, PAGE_NUMBER)
     response = get_response(URL, payload)
     soup_object = make_soup(response)
-    result_list = extract_data(soup_object, PARENT_CSS[0], PARENT_CSS[1])
+    result_list = extract_table(soup_object)
+    data = extract_col(result_list)
+    
 
-    print(result_list) # !TODO: extract relevant info from this list 
-
+    print(data) # !TODO: extract relevant info from this list 
+    print(type(data))
     print('=========================================')
 
